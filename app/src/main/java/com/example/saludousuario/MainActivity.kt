f699fdd48b38b990fd919ee0934a9f5f6c78b9f4
Harold Rodas <haroldrodas044@gmail.com>
2023-03-10 16:05:53 -0500

    Subiendo el proyecto

---
+++ app/src/main/java/com/example/saludousuario/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.saludousuario
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val boton:Button = findViewById(R.id.btnIngresar)
+        boton.setOnClickListener {componentes()}
+    }
+
+    private fun componentes(){
+        //Nombre
+        val fieldTxt:EditText= findViewById(R.id.txtNombre)
+        val name:String=fieldTxt.text.toString()
+
+        //Radio
+        val radioOne:RadioButton =findViewById(R.id.radioButton)
+        val radioTwo:RadioButton =findViewById(R.id.radioButton2)
+        var radioResult: String = ""
+
+        if(radioOne.isChecked){
+            radioResult += "opcion 1 seleccionada "
+        }else{
+            radioResult += "opcion 1 no seleccionada"
+        }
+
+        if(radioTwo.isChecked) {
+            radioResult += "opcion 2 seleccionada "
+        }else{
+            radioResult += "opcion 2 no seleccionada"
+        }
+
+        //CheckBox
+        val checkOne:CheckBox =findViewById(R.id.checkBox2)
+        val checkTwo:CheckBox =findViewById(R.id.checkBox3)
+        var checkResult: String = ""
+
+        if (checkOne.isChecked){
+            checkResult += "Check 1 activo "
+        }else{
+            checkResult += "Check 1 inactivo "
+        }
+        if (checkTwo.isChecked) {
+            checkResult += "Check 2 activo "
+        }else{
+            checkResult += "Check 2 inactivo "
+        }
+
+        //Toggle
+        val toggleBtn:ToggleButton = findViewById(R.id.toggleButton)
+        var statusToggle:String = ""
+
+        if (toggleBtn.isChecked) {
+            statusToggle += "Toogle activo"
+        } else {
+            statusToggle += "Toogle inactivo"
+        }
+
+        //Switch
+        val switchBtn:Switch = findViewById(R.id.switch2)
+        var statusSwitch:String = ""
+        if(switchBtn.isChecked) {
+            statusSwitch += "Switch activo"
+        } else {
+            statusSwitch += "Switch inactivo"
+        }
+
+        val message = "$name, $radioResult, $checkResult, $statusToggle, $statusSwitch"
+        Toast.makeText(this, message , Toast.LENGTH_LONG).show()
+    }
+}
+
+
+
